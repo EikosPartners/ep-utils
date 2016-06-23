46eb3511808db53e92f9748ce370aa0744b599e5
Peter Fatyga <peter.fatyga@eikospartners.com>
2016-06-23 10:35:04 -0400

    Remove logger, rename files

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var path = require('path');
 
 module.exports = {
   fsWrapper: require(path.join(__dirname, 'src/fsWrapper.js')),
-  logger: require(path.join(__dirname, 'src/logger.js')),
   sortAndFilter: require(path.join(__dirname, 'src/sortAndFilter.js')),
   dataUtils: require(path.join(__dirname, 'src/dataUtils.js')),
   setCacheHeaders: require(path.join(__dirname, 'src/setCacheHeaders.js'))
